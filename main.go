0112493cbee2efadb3f631db5acf840cb7281bf1
abhik <abhiknvarma@gmail.com>
2024-09-16 22:14:18 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv"
+)
+
+const (
+	baseUrl = "https://openexchangerates.org/api/latest.json"
+	refresh = 3 * time.Hour
+)
+
+type Rates struct {
+	INR float64 `json:"INR"`
+	USD float64 `json:"USD"`
+	JPY float64 `json:"JPY"`
+}
+
+var (
+	rates      Rates
+	ratesMutex sync.RWMutex
+	apiKey     string
+)
+
+func main() {
+	apiKey = os.Getenv("OPENEXCHANGERATES_API_KEY")
+	if apiKey == "" {
+		log.Fatal("OPENEXCHANGERATES_API_KEY env var not set :(")
+	}
+
+	loadRates()
+
+	fetchRates()
+
+	go func() {
+		ticker := time.NewTicker(refresh)
+		for range ticker.C {
+			fetchRates()
+		}
+	}()
+
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/static", "./static")
+
+	r.GET("/", indexHandler)
+
+	r.Run(":8080")
+}
+
+func fetchRates() {
+	url := fmt.Sprintf("%s?app_id=%s&base=USD&symbols=INR,JPY", baseUrl, apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("Error fetching rates:", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var result struct {
+		Rates Rates `json:"rates"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		log.Println("Error decoding response:", err)
+		return
+	}
+
+	ratesMutex.Lock()
+	rates = result.Rates
+	rates.USD = 1
+	ratesMutex.Unlock()
+
+	fmt.Println("Rates updated:", rates)
+	storeRates()
+}
+
+func storeRates() {
+	ratesMutex.RLock()
+	defer ratesMutex.RUnlock()
+
+	data, err := json.Marshal(rates)
+	if err != nil {
+		log.Println("Error marshalling rates:", err)
+		return
+	}
+
+	err = os.WriteFile("rate.json", data, 0644)
+	if err != nil {
+		log.Println("Error writing rates to files:", err)
+	}
+}
+
+func loadRates() {
+	data, err := os.ReadFile("rates.json")
+	if err != nil {
+		log.Println("Error reading rates file:", err)
+		return
+	}
+
+	ratesMutex.Lock()
+	defer ratesMutex.Unlock()
+
+	err = json.Unmarshal(data, &rates)
+	if err != nil {
+		log.Println("Error unmarshalling rates:", err)
+	} else {
+		log.Println("Loaded stored rates:", rates)
+	}
+}
+
+func indexHandler(c *gin.Context) {
+	ratesMutex.RLock()
+	defer ratesMutex.RUnlock()
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"rates": rates,
+	})
+}
